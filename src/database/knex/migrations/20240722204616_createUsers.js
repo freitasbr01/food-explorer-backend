9c3cbe4595f317128031282430cbf34d2c9a0ca5
Alan <freitasbr01@gmail.com>
2024-08-10 15:00:37 -0300

    updated

--- src/database/knex/migrations/20240722204616_createUsers.js
+++ src/database/knex/migrations/20240722204616_createUsers.js
@@ -5,7 +5,7 @@ exports.up = knex => knex.schema.createTable('users', table => {
   table.text('password').notNullable();
 
   table.enum("role", ["admin", "customer"], { useNative: true, enumName: "roles" })
-  .notNullable().default("customer");
+  .notNullable().default("admin");
 
   table.timestamp('created_at').defaultTo(knex.fn.now()); 
   table.timestamp('updated_at').defaultTo(knex.fn.now()); 
