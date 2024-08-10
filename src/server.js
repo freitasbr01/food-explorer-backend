eb2add9ce1030b3a9ce2f8fba322e4a5edf62dc6
Alan <freitasbr01@gmail.com>
2024-08-10 14:46:02 -0300

    updated

--- src/server.js
+++ src/server.js
@@ -15,7 +15,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://food-explorer01.netlify.app"
+  ],
   credentials: true
 }));
 
