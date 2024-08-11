e27e84e41ef73c1dc1913ec84089d096f29f9cda
Alan <freitasbr01@gmail.com>
2024-08-11 10:26:52 -0300

    updated

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   jwt: {
     secret: process.eventNames.AUTH_SECRET || "default",
-    expiresIn: "1d"
+    expiresIn: "24h"
   }
 }
--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -3,16 +3,17 @@ const AppError = require("../utils/AppError");
 const authConfig = require("../configs/auth");
 
 function ensureAuthenticated(request, response, next) {
-  const authHeader = request.headers;
+  const authHeader = request.headers.cookie;
 
- console.log(authHeader.cookie)
-
- 
-  if(!authHeader.cookie) {
+  if (!authHeader) {
     throw new AppError("JWT Token não informado", 401);
   }
 
-  const [, token] = authHeader.cookie.split('token=');
+  const token = authHeader.split('token=')[1];
+
+  if (!token) {
+    throw new AppError("JWT Token não encontrado no cookie", 401);
+  }
 
   try {
     const { role, sub: user_id } = verify(token, authConfig.jwt.secret);
@@ -26,7 +27,43 @@ function ensureAuthenticated(request, response, next) {
   } catch {
     throw new AppError('Invalid JWT token', 401);
   }
-
 }
 
 module.exports = ensureAuthenticated;
+
+
+
+
+
+// const { verify } = require("jsonwebtoken");
+// const AppError = require("../utils/AppError");
+// const authConfig = require("../configs/auth");
+
+// function ensureAuthenticated(request, response, next) {
+//   const authHeader = request.headers;
+
+//  console.log(authHeader.cookie)
+
+ 
+//   if(!authHeader.cookie) {
+//     throw new AppError("JWT Token não informado", 401);
+//   }
+
+//   const [, token] = authHeader.cookie.split('token=');
+
+//   try {
+//     const { role, sub: user_id } = verify(token, authConfig.jwt.secret);
+    
+//     request.user = {
+//       id: Number(user_id),
+//       role
+//     };
+
+//     return next();
+//   } catch {
+//     throw new AppError('Invalid JWT token', 401);
+//   }
+
+// }
+
+// module.exports = ensureAuthenticated;
